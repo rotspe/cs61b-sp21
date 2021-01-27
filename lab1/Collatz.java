301773ce2a55817103719bc2e4050688e298a8e4
rotspe <78112522+rotspe@users.noreply.github.com>
2021-01-27 23:08:00 +0200

    done with Collatz

---
+++ lab1/Collatz.java
@@ -0,0 +1,22 @@
+/** Class that prints the Collatz sequence starting from a given number.
+ *  @author Rossen Petrov (rossen.t.petrov@gmail.com)
+ */
+public class Collatz {
+    public static int nextNumber(int n) {
+        if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return 3 * n + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        while (n != 1) {
+            System.out.print(n + " ");
+            n = nextNumber(n);
+        }
+        System.out.print(n);
+    }
+}
+
